359d44049420710ec7ce50c1a0076361d6bcdefd
Sanchit <sanchit4242@gmail.com>
2022-08-13 12:08:49 +0530

    url

--- ansible/deployBlimp.go
+++ ansible/deployBlimp.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os/exec"
 
 	"github.com/gorilla/mux"
 )
@@ -20,6 +19,7 @@ type BlimpUser struct {
     AllocationId            string `json:"AllocationId"`
     Port                    string `json:"Port"`
     ConsoleBlimp            string `json:"ConsoleBlimp"`
+    Url                     string `json:"Url"`
 }
 
 func DeployBlimp(w http.ResponseWriter, r *http.Request) {
@@ -32,20 +32,21 @@ func DeployBlimp(w http.ResponseWriter, r *http.Request) {
     json.Unmarshal(reqBody, &newUser)
     fmt.Println("got params")
 
-    cmd := exec.Command("sudo", "bash", "./minio_script.sh", newUser.IP, newUser.Server_UserName, 
-    newUser.ConfigurationDirectory, newUser.MinioUserName, newUser.MinioPassword, 
-    newUser.AllocationId, newUser.ConsoleBlimp, newUser.Port)
+    // cmd := exec.Command("sh", "./minio_script.sh", newUser.IP, newUser.Server_UserName, 
+    // newUser.ConfigurationDirectory, newUser.MinioUserName, newUser.MinioPassword, 
+    // newUser.AllocationId, newUser.ConsoleBlimp, newUser.Port)
         
-    // bash minio_script.sh 3.144.74.110 root $HOME/.zcn manali manalipassword 773dde936212cb60b312b1577a7a21aae4a4114b7ece242b8c2be5851b3656c4
-    // bash minio_script.sh IP server_Username configDirectory miniousername miniopassword allocationID port console
+    // // bash minio_script.sh 3.144.74.110 root $HOME/.zcn manali manalipassword 773dde936212cb60b312b1577a7a21aae4a4114b7ece242b8c2be5851b3656c4
+    // // bash minio_script.sh IP server_Username configDirectory miniousername miniopassword allocationID port console
     
-    out, err := cmd.CombinedOutput()
-    if err != nil {
-        log.Fatal(err.Error())
-    }
+    // out, err := cmd.CombinedOutput()
+    // if err != nil {
+    //     log.Fatal(err.Error())
+    // }
+    newUser.Url = "http://" + newUser.IP + ":9000"
     w.WriteHeader(http.StatusCreated)
     json.NewEncoder(w).Encode(newUser)
-    fmt.Println(string(out))
+    //fmt.Println(string(out))
 }
 
 func main() {
