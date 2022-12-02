49d08dc31307e8dfed8515d43d53b8b8716bcac6
ahmedzidan <ahmedzidan_2015@hotmail.com>
2022-12-03 02:19:07 +0800

    add docker images from 0cain

--- client-api/main.go
+++ client-api/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {})
 	mux.HandleFunc("/", s3api.Handler)
 	err := http.ListenAndServe(":3001", mux)
 	if err != nil {
