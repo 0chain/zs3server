eebb84aae37aacf9468ee9ff5c3d07f365885a3a
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-04-30 04:14:11 +0530

    Added check before creating bucket

--- client-api/s3api/create_bucket.go
+++ client-api/s3api/create_bucket.go
@@ -25,20 +25,27 @@ func createBucket(bucketName string, location string, minioCredentials MinioCred
 		return createBucketResponse, err
 	}
 
-	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
-	if err != nil {
-		// Check to see if we already own this bucket (which happens if you run this twice)
-		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
-		if errBucketExists == nil && exists {
-			log.Printf("We already own %s\n", bucketName)
-			return createBucketResponse, nil
-		} else {
-			//log.Fatalln(err)
-			return createBucketResponse, err
+	exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+	if errBucketExists == nil && exists {
+		log.Printf("We already own %s\n", bucketName)
+		return createBucketResponse, nil
+	} else {
+		//log.Fatalln(err)
+		err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+		if err != nil {
+			// Check to see if we already own this bucket (which happens if you run this twice)
+			exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+			if errBucketExists == nil && exists {
+				log.Printf("We already own %s\n", bucketName)
+				return createBucketResponse, nil
+			} else {
+				//log.Fatalln(err)
+				return createBucketResponse, err
+			}
 		}
-	}
-	createBucketResponse.Bucketname = bucketName
-	createBucketResponse.Success = true
+		createBucketResponse.Bucketname = bucketName
+		createBucketResponse.Success = true
 
-	return createBucketResponse, nil
+		return createBucketResponse, nil
+	}
 }
