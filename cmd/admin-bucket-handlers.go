6b041ada095b3079d028e5ccf3e3f790f9d2c218
Hitenjain14 <hitenjain146@gmail.com>
2024-03-04 11:17:48 +0530

    add remote target log

--- cmd/admin-bucket-handlers.go
+++ cmd/admin-bucket-handlers.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -177,6 +178,7 @@ func (a adminAPIHandlers) SetRemoteTargetHandler(w http.ResponseWriter, r *http.
 		writeErrorResponseJSON(ctx, w, errorCodes.ToAPIErrWithErr(ErrAdminConfigBadJSON, err), r.URL)
 		return
 	}
+	log.Println("remoteTargetBucket: ", bucket, update, target.Arn)
 	if update {
 		// overlay the updates on existing target
 		tgt := globalBucketTargetSys.GetRemoteBucketTargetByArn(ctx, bucket, target.Arn)
