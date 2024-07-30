f669e30e9ad97fe15e91a99f8dd476dd987dcb62
harsh4723 <harsh4723@rediffmail.com>
2024-07-30 18:57:19 +0530

    feat: add remove object form cache

--- client-api/s3api/remove_object.go
+++ client-api/s3api/remove_object.go
@@ -23,10 +23,21 @@ func removeObject(bucketName string, objectName string, minioCredentials MinioCr
 	if err != nil {
 		return removeObjectResponse, err
 	}
+	minioCacheClient, err := minio.New("miniocache:9000", &minio.Options{
+		Creds:  credentials.NewStaticV4(minioCredentials.AccessKey, minioCredentials.SecretAccessKey, ""),
+		Secure: USESSL,
+	})
+	if err != nil {
+		return removeObjectResponse, err
+	}
 	err = minioClient.RemoveObject(ctx, bucketName, objectName, minio.RemoveObjectOptions{GovernanceBypass: true})
 	if err != nil {
 		fmt.Println(err)
 		return removeObjectResponse, err
+	}
+	err = minioCacheClient.RemoveObject(ctx, bucketName, objectName, minio.RemoveObjectOptions{GovernanceBypass: true})
+	if err != nil {
+		fmt.Println("Cache not present", err)
 	}
 	removeObjectResponse.Success = true
 	removeObjectResponse.ObjectName = objectName
