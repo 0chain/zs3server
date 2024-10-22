f848baebb8ba64a1aa0bd7d5a697ed2fbb73f3a2
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-10-23 02:15:37 +0530

    feature/refactor-gosdk

--- cmd/gateway/zcn/multipart-test/s3gateway/zcn/initSDK.go
+++ cmd/gateway/zcn/multipart-test/s3gateway/zcn/initSDK.go
@@ -2,6 +2,7 @@ package zcn
 
 import (
 	"fmt"
+	"github.com/0chain/gosdk/core/client"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -9,7 +10,6 @@ import (
 
 	"github.com/0chain/gosdk/core/conf"
 	"github.com/0chain/gosdk/core/logger"
-	"github.com/0chain/gosdk/zboxcore/blockchain"
 	"github.com/0chain/gosdk/zboxcore/sdk"
 	"github.com/0chain/gosdk/zcncore"
 	"github.com/mitchellh/go-homedir"
@@ -59,40 +59,16 @@ func InitializeSDK(configDir, allocid string, nonce int64) error {
 		return err
 	}
 
-	network, _ := conf.LoadNetworkFile(filepath.Join(configDir, "network.yaml"))
-	if network.IsValid() {
-		zcncore.SetNetwork(network.Miners, network.Sharders)
-		conf.InitChainNetwork(&conf.Network{
-			Miners:   network.Miners,
-			Sharders: network.Sharders,
-		})
-	}
-
 	logger.SyncLoggers([]*logger.Logger{zcncore.GetLogger(), sdk.GetLogger()})
 	zcncore.SetLogFile("cmdlog.log", true)
 	sdk.SetLogFile("cmd.log", true)
 
-	err = zcncore.InitZCNSDK(cfg.BlockWorker, cfg.SignatureScheme,
-		zcncore.WithChainID(cfg.ChainID),
-		zcncore.WithMinSubmit(cfg.MinSubmit),
-		zcncore.WithMinConfirmation(cfg.MinConfirmation),
-		zcncore.WithConfirmationChainLength(cfg.ConfirmationChainLength))
+	err = client.InitSDK(string(walletBytes), cfg.BlockWorker, cfg.ChainID, cfg.SignatureScheme, nonce, false, true, cfg.MinSubmit, cfg.MinConfirmation, cfg.ConfirmationChainLength, cfg.SharderConsensous)
 	if err != nil {
 		return err
 	}
 
-	err = sdk.InitStorageSDK(string(walletBytes), cfg.BlockWorker, cfg.ChainID, cfg.SignatureScheme, cfg.PreferredBlobbers, nonce)
-	if err != nil {
-		return err
-	}
-
-	blockchain.SetMaxTxnQuery(cfg.MaxTxnQuery)
-	blockchain.SetQuerySleepTime(cfg.QuerySleepTime)
 	conf.InitClientConfig(&cfg)
-
-	if network.IsValid() {
-		sdk.SetNetwork(network.Miners, network.Sharders)
-	}
 
 	sdk.SetNumBlockDownloads(10)
 	return nil
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ toolchain go1.22.5
 require (
 	cloud.google.com/go/storage v1.28.1
 	github.com/0chain/errors v1.0.3
-	github.com/0chain/gosdk v1.18.0-RC4
+	github.com/0chain/gosdk v1.18.0-RC5
 	github.com/Azure/azure-pipeline-go v0.2.2
 	github.com/Azure/azure-storage-blob-go v0.10.0
 	github.com/Shopify/sarama v1.28.0
