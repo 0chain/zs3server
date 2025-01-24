0e8b38ac288cd1dbb4c8babfb7e8fc90fa55babf
Hitenjain14 <hitenjain146@gmail.com>
2025-01-24 16:25:01 +0530

    decrease workers

--- internal/config/cache/lookup.go
+++ internal/config/cache/lookup.go
@@ -260,7 +260,7 @@ func LookupConfig(kvs config.KVS) (Config, error) {
 			return cfg, config.ErrInvalidUploadQueueTh(err)
 		}
 	}
-	cfg.UploadWorkers = 100
-	cfg.UploadQueueTh = 100
+	cfg.UploadWorkers = 50
+	cfg.UploadQueueTh = 50
 	return cfg, nil
 }
