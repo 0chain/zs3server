2dbdf5bbceb328188c40331c2d53025c73eb3565
Hitenjain14 <hitenjain146@gmail.com>
2025-02-26 17:31:33 +0530

    process half batch

--- cmd/gateway/zcn/worker.go
+++ cmd/gateway/zcn/worker.go
@@ -33,13 +33,20 @@ func IntiBatchUploadWorkers(ctx context.Context, alloc *sdk.Allocation, waitTime
 				// process the batch upload or wait for more operations
 				opRequest = append(opRequest, op)
 				if len(opRequest) == maxOperations || iterations > 2 {
-					log.Println("process batch for time condition")
+					log.Println("process batch for time condition", iterations)
 					workerChan <- opRequest
 					opRequest = make([]sdk.OperationRequest, 0, 5)
 					iterations = 0
 					continue
 				}
 				if len(batchUploadChan) == 0 {
+					if len(opRequest) > maxOperations/2 {
+						log.Println("processing half batch")
+						workerChan <- opRequest
+						opRequest = make([]sdk.OperationRequest, 0, 5)
+						iterations = 0
+						continue
+					}
 					// wait for more operations
 					log.Println("waiting for more operations: ", len(opRequest))
 					iterations++
